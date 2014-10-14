aefc8983c75193391619886d49825f559614ba7e
jiaoda <jiaoda@jiaodatekiMacBook-Pro.local>
2014-10-14 21:55:01 +0800

    Initial Commit

---
+++ DesignPattern_cpp/DesignPattern_cpp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DesignPattern_cpp
+//
+//  Created by jiaoda on 14-10-14.
+//  Copyright (c) 2014年 NCET. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
